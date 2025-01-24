ecb7352f45cae60bf007c20d3198b9d9ed94fa59
JIBRAN SHAHAB SHEIKH <jibransheikh616@gmail.com>
2025-01-24 23:02:50 +0500

    Add files via upload

---
+++ contditinal.py
@@ -0,0 +1,75 @@
+#if 3 ==3:
+    #print("Graeter")
+
+#Number = int(input("Enter a Number: "))
+# if Number Greater than 10 or Equal 10 answer Good
+#if Number >=10:
+    #print("Good")
+
+#else:
+   # print("Bad")
+
+#Total = int(input("Enter a Number: "))
+
+#if Total >= 10000:
+    #print("Eligible for Discount!")
+
+#elif Total >=1 and Total <=9999:
+    #print("You are not eligible!")
+
+#else:
+    #print("Please enter right amount!")    
+
+        
+#power statment
+#def_init_(self):
+#Default state: k electric is off ,ups is on
+#self.k_electric = False
+#self.ups= True
+
+
+
+# Ups = input("Enter Off/on:")
+# K_Electric = input("Enter Off/On:")
+
+# if K_Electric == Ups:
+    # print(Good)
+
+#if i in range("K_Electric, Ups,"):
+    #print(K_Electric,Ups)
+    
+# x = int(input("Enter a Number"))
+
+# if x >= 90: 
+    # print("Grad A") 
+# elif x >= 80:
+    # print("Grad B")
+# elif x >= 70:
+    # print("Grad C")    
+
+# else:
+    # print("Fail")    
+
+B = str(input("Welcome" " "))
+
+
+if B == "Hi":
+    print("Hello")
+else:
+    print("Great")    
+B = (input("Ready to Answer" " "))    
+if B == "yes":
+    print("Great")  
+
+B = str(input("Who is the founder of pakistan?" " " ))  
+if B == "Quid e Azam":
+    print("Correct Answer" )
+B = str(input("Ready to next Question?" " "))
+if B == "yes":  
+    print("You are intelligent")
+    print("Your Score is 10")   
+B = str(input("What is the capital of Pakistan.?" " "))
+if B == "Islamabad":
+    print("Correct Answer. Your Score has been 20") 
+else:
+    print("Failed. Try again")        
