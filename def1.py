53b1a803379416d8c5703c80bacc5868a59783db
JIBRAN SHAHAB SHEIKH <jibransheikh616@gmail.com>
2025-01-24 23:00:50 +0500

    Add files via upload

---
+++ def1.py
@@ -0,0 +1,44 @@
+# Defination used function (def) Dynamic function.
+# Used Paramater & Argument
+# def Robot_2(Name,Age,Adress):
+    # print(Name,Age,Adress)
+
+# Robot_2("Jibran Shahab",26,"A/8-Falak Naaz Shahara-e-Faisal karachi" "")    
+
+
+# def Robot_sum():
+
+
+        
+    #x = int(input("Enter a 1st Number of Multiply" " "))
+    # y = int(input("Enter a 2nd Number of Multiply" " "))
+    # z = x*y
+    # print("Two number of Multiply =", z, )
+
+    # x = int(input("Enter a 1st Number of divided" " "))
+    # y = int(input("Enter a 2nd Number of divided" " "))
+    # z = x/y
+    # print("Two number of Divided =", z, )
+
+    # x = int(input("Enter a 1st Number of Sum " " "))
+    # y = int(input("Enter a 2nd Number of sum" " "))
+    # z = x+y
+    # print("Two number of sum =", z, )
+    # print("Thank you")
+# Call to Function Robot_sum ()...
+
+# Robot_sum()
+
+def Happy_Birth():
+    x = str(input("Enter a Name" " "))
+    if x == "Osama":
+        print("Happy Birth day to you! ****Osama**** ")
+    else:
+        print("Not for You dear!")    
+    print("Happy Birth day to you!")
+    print("Happy Birth day to you!")
+    print("Happy Birth day to you!")
+    print("Happy Birth day to you!")
+    print("Happy Birth day to you!")
+
+Happy_Birth()
