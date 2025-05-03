32f4f3c410b94c9b551770603696fa93e846c3b3
JIBRAN SHAHAB SHEIKH <jibransheikh616@gmail.com>
2025-05-04 02:21:56 +0500

    Delete Madlib.py

--- Madlib.py
+++
@@ -1,9 +0,0 @@
-# Get input from the user
-name = input("What is your name? ")
-age = input("How old are you? ")
-
-# Convert age to an integer
-age = int(age)
-
-# Print a message using an f-string
-print(f"Hello, {name}! You are {age} years old.")
